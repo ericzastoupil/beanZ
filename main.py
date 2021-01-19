6870c62401ac9cb57b956b061f6a75830a2c64d8
ericzastoupil <ericzastoupil@users.noreply.github.com>
2021-01-18 20:18:34 -1000

    figuring out easygui

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
+import easygui
 import beancount
 import importers.amex_importer
 import importers.usaa_importer
 
 if __name__ == '__main__':
 	print("Hello World")
+
+	'''if os.environ.get('DISPLAY','')=='':
+		print('no display found, using :0.0')
+		os.environ.__setitem__('DISPLAY',':0.0')
+	easygui.ynbox('Stuff or things?', 'Title', ('Stuff', 'Things'))
+	'''
