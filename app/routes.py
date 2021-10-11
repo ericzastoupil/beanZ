d46160740949a6ecab6a595cf83cda155657ff47
ericzastoupil <ericzastoupil@users.noreply.github.com>
2021-10-10 21:34:22 -1000

    more progress on login

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
+#Main Page
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Eric'}
     return render_template('index.html', title='Home', user=user)
+
+#Login Page
+@app.route('/login', methods=['GET', 'POST']) #POSTs are for browser submitting form data to the server
+def login():
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    
+    return render_template('login.html', title='Sign In', form=form)
