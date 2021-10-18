a7e35742a46d6b9d6f625144b412f127bbf61a4c
ericzastoupil <ericzastoupil@users.noreply.github.com>
2021-10-17 14:14:54 -1000

    implementing full db model

--- app/models.py
+++ app/models.py
@@ -8,50 +8,56 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    #transactions = db.relationship('Transaction', backref='user', lazy='dynamic')
+    transactions = db.relationship('Transaction', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return "<User {}".format(self.username)
 
-'''
+#Transaction table definition
 class Transaction(db.Model):
     __tablename__ = 'transaction'
     transaction_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     date_ingested = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     date_transaction = db.Column(db.DateTime, index=True)
-    #account_id = db.Column(db.Integer, ForeignKey('account.account_id'))
-    #merchant_id = db.Column(db.Integer, ForeignKey('merchant.merchant_id'))
+    account_id = db.Column(db.Integer, db.ForeignKey('account.account_id'))
+    merchant_id = db.Column(db.Integer, db.ForeignKey('merchant.merchant_id'))
     desc = db.Column(db.String(120))
+
+    def __repr__(self):
+        return "<Transaction {}>".format(self.date_transaction, self.desc)
+        
+#Account table definition
+class Account(db.Model):
+    __tablename__ = 'account'
+    account_id = db.Column(db.Integer, primary_key=True)
+    institution_id = db.Column(db.Integer, db.ForeignKey('institution.institution_id'))
+    type_id = db.Column(db.Integer, db.ForeignKey('account_type.acount_type_id'))
 
     def __repr__(self):
         return "<Transaction {}".format(self.date_transaction, self.desc)
 
-Class Account(db.Model):
-    __tablename__ = 'account'
-    account_id = db.Column(db.Integer, primary_key=True)
-    #institution_id = db.Column(db.Integer, ForeignKey('institution.institution_id'))
-    #type_id = db.Column(db.Integer, ForeignKey('account_type.acount_type_id'))
-
-Class Instutition(db.Model):
+#Institution table definition
+class Instutition(db.Model):
     __tablename__ = 'institution'
     institution_id = db.Column(db.integer, primary_key=True)
-    #institution_name = db.Column(db.String(64), index=True)
+    institution_name = db.Column(db.String(64), index=True)
 
-Class AccountType(db.Model):
+#AccountType table definition
+class AccountType(db.Model):
     __tablename__ = 'account_type'
     account_type_id = db.Column(db.integer, primary_key=True)
-    #account_type = db.Column(db.String(64), index=True)
+    account_type = db.Column(db.String(64), index=True)
 
-Class TransactionTag(db.Model)
+#TransactionTag table definition
+class TransactionTag(db.Model):
     __tablename__ = 'transaction_tag'
     tag_id = db.Column(db.Integer, primary_key=True)
-    #transaction_id = db.Column(db.Integer, ForeignKey('transaction.transaction_id'))
-    #tag_main_type_id = db.Column(db.Integer, ForeignKey('tag_main_type.tag_main_type.id'))
-    #tag = db.Column(db.String(64))
+    transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.transaction_id'))
+    tag_main_type_id = db.Column(db.Integer, db.ForeignKey('tag_main_type.tag_main_type.id'))
+    tag = db.Column(db.String(64))
 
-Class Merchant(db.Model)
+#Merchant table definition
+class Merchant(db.Model):
     __tablename__ = 'merchant'
     merchant_id = db.Column(db.String(64), primary_key=True)
-
-'''
