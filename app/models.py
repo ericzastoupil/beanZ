91606307c87689ee7fb097cf5263d474814f8aa8
ericzastoupil <ericzastoupil@users.noreply.github.com>
2021-10-23 20:14:06 -1000

    added set password and check password

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 #User table definition
 class User(db.Model):
@@ -12,6 +13,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}- email: {self.email}"
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 #Transaction table definition
 class Transaction(db.Model):
