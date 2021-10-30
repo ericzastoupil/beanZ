c5bc465df35b6599694622e3181fd2430840de91
ericzastoupil <eric.zastoupil@gmail.com>
2021-10-29 21:29:46 -1000

    User table now has last_seen

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     transactions = db.relationship('Transaction', backref='user', lazy='dynamic')
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"<User {self.username}- email: {self.email}"
