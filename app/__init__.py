f7b75d49d2e676a844c212a02ccb70b0e95021cf
ericzastoupil <eric.zastoupil@gmail.com>
2021-11-28 12:04:07 -1000

    finally linked the CSS correctly. used static folder

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,7 @@ if not app.debug:
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
+    app.static_folder = 'static'
     app.logger.setLevel(logging.INFO)
     app.logger.info('beanZ startup')
 
