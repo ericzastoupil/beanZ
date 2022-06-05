9b74e69b8ad3ee855ce5bd686a7cd3ab9af117f2
ericzastoupil <eric.zastoupil@gmail.com>
2022-06-05 11:22:43 -1000

    fixed last_seen

--- app/routes.py
+++ app/routes.py
@@ -74,5 +74,5 @@ def user(username):
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow
+        current_user.last_seen = datetime.utcnow()
         db.session.commit()
