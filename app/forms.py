32f66ef2b7c7e0bdfa946a445ecb2dade9edc8a9
ericzastoupil <eric.zastoupil@gmail.com>
2022-06-15 18:42:46 -1000

    starting to add password reset functionality WIP

--- app/forms.py
+++ app/forms.py
@@ -63,6 +63,11 @@ class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
 
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
+
 '''
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
