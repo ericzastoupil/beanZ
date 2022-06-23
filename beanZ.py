363350cfb63d20c1a18d0d76d04a0c3f62345c26
ericzastoupil <eric.zastoupil@gmail.com>
2022-06-22 18:29:18 -1000

    minor change

--- beanZ.py
+++ beanZ.py
@@ -3,7 +3,6 @@ from app.models import User, Transaction
 
 app = create_app()
 
-
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Transaction': Transaction}
