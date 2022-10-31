bad9d1c13a70d66c5d3892389dc9acc321aba5a9
ericzastoupil <eric.zastoupil@gmail.com>
2022-10-30 20:49:27 -1000

    added usedrname back in

--- app/models.py
+++ app/models.py
@@ -12,6 +12,8 @@ class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    #first_name = db.Column(db.String(64), index=True, unique=True)
+    #last_name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     transactions = db.relationship('Transaction', backref='user', lazy='dynamic')
@@ -55,6 +57,11 @@ class Transaction(db.Model):
 
     def __repr__(self):
         return f"<Transaction {self.date_transaction}- {self.desc}>"
+
+    '''
+    def store_transaction(self, trans):
+        self.
+    '''
         
 #Account table definition
 class Account(db.Model):
