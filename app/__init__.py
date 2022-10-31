ed068f431c03c1a738fe6426cd306cdfac0b30bd
ericzastoupil <eric.zastoupil@gmail.com>
2022-10-30 20:50:03 -1000

    changed config back

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)
